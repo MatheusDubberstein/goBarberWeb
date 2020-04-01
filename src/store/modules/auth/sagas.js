5985d2eb5fae6ee03b45b367797d819486c4c18c
matheusDubberstein <matheusdub.s@gmail.com>
2020-04-01 10:52:31 -0300

    add: authorization call in api

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -21,7 +21,10 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Barer ${token}`;
+
     yield put(signInSuccess(token, user));
+
     history.push('/dashboard');
   } catch (error) {
     toast.error('Falha na autenticação, verifique seus dados.');
@@ -29,4 +32,37 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha no cadastro, verifique os seus dados!');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Barer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
